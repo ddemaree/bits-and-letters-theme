59b87933fb11fbbf2aafe47a71013b18cc7a2fe9
David Demaree <david@demaree.me>
2019-08-18 19:29:49 -0400

    Initial commit

---
+++ template-parts/post/discussion-meta.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template for displaying Current Discussion on posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0.0
+ */
+
+/* Get data from current discussion on post. */
+$discussion    = bitsandletters_get_discussion_data();
+$has_responses = $discussion->responses > 0;
+
+if ( $has_responses ) {
+	/* translators: %1(X comments)$s */
+	$meta_label = sprintf( _n( '%d Comment', '%d Comments', $discussion->responses, 'bitsandletters' ), $discussion->responses );
+} else {
+	$meta_label = __( 'No comments', 'bitsandletters' );
+}
+?>
+
+<div class="discussion-meta">
+	<?php
+	if ( $has_responses ) {
+		bitsandletters_discussion_avatars_list( $discussion->authors );
+	}
+	?>
+	<p class="discussion-meta-info">
+		<?php echo bitsandletters_get_icon_svg( 'comment', 24 ); ?>
+		<span><?php echo esc_html( $meta_label ); ?></span>
+	</p>
+</div><!-- .discussion-meta -->
